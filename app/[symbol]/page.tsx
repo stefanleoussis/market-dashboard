459e1b66818317ce4053cc763e5de8edd577dff1
Stefan <58789967+stefanleoussis@users.noreply.github.com>
2025-11-07 01:29:46 -0800

    Initialize Next.js project with app router

---
+++ app/[symbol]/page.tsx
@@ -0,0 +1,3 @@
+export default function StockDetails() {
+  return <div></div>;
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+export default function Dashboard() {
+  return (
+    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'></div>
+  );
+}
